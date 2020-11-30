4e2ae755ddeb79e89cdb9e6f711b46814f729cc5
GuilhermeLunetta <49534365+GuilhermeLunetta@users.noreply.github.com>
2020-11-29 21:13:59 -0300

    test4

--- src/pages/deleteAccount.js
+++ src/pages/deleteAccount.js
@@ -123,7 +123,7 @@ export { GetNames }
 export default function deleteAccount(props) {
   var id = ""
   if (props.location != undefined) {
-    id = props.location.state.idUser
+    id = props.location.state.personId
   }
 
   require("../components/pokedex.css")
